a8b2afe857f2734d1d183f7adf5a3c1ca04a5b52
robkeevil <robkeevil@gmail.com>
2024-02-10 10:36:56 +0100

    fix unit tests

--- scripts/run-unit-tests.sh
+++ scripts/run-unit-tests.sh
@@ -15,9 +15,9 @@ echo "Downloading the models required for testing from the huggingface hub"
 
 if [[ ! -d "$src_dir/models" ]]; then
     mkdir -p $src_dir/models
-    (cd $src_dir/models && git clone https://huggingface.co/KnightsAnalytics/all-MiniLM-L6-v2 &&\
+    (cd $src_dir/models && git clone https://huggingface.co/KnightsAnalytics/all-MiniLM-L6-v2 && \
     git clone https://huggingface.co/KnightsAnalytics/distilbert-base-uncased-finetuned-sst-2-english && \
-    git clone git clone https://huggingface.co/KnightsAnalytics/distilbert-NER)
+    git clone https://huggingface.co/KnightsAnalytics/distilbert-NER)
 fi
 
 # build with compose
