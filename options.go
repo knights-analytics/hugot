96ce9b792177eb931b06ab144b3ededfeb306fa8
robkeevil <robkeevil@gmail.com>
2024-04-17 15:16:15 +0200

    Work to integrate the various options of execution providers

--- options.go
+++ options.go
@@ -1,14 +1,24 @@
 package hugot
 
 type ortOptions struct {
-	libraryPath       string
-	telemetry         bool
-	intraOpNumThreads int
-	interOpNumThreads int
-	cpuMemArena       bool
-	cpuMemArenaSet    bool
-	memPattern        bool
-	memPatternSet     bool
+	libraryPath        string
+	telemetry          bool
+	intraOpNumThreads  int
+	interOpNumThreads  int
+	cpuMemArena        bool
+	cpuMemArenaSet     bool
+	memPattern         bool
+	memPatternSet      bool
+	cudaOptions        map[string]string
+	cudaOptionsSet     bool
+	coreMLOptions      uint32
+	coreMLOptionsSet   bool
+	directMLOptions    int
+	directMLOptionsSet bool
+	openVINOOptions    map[string]string
+	openVINOOptionsSet bool
+	tensorRTOptions    map[string]string
+	tensorRTOptionsSet bool
 }
 
 // WithOption is the interface for all option functions
@@ -60,5 +70,66 @@ func WithMemPattern(enable bool) WithOption {
 	return func(o *ortOptions) {
 		o.memPattern = enable
 		o.memPatternSet = true
+	}
+}
+
+// WithCuda Use this function to set the options for CUDA provider.
+// It takes a pointer to an instance of CUDAProviderOptions struct as input.
+// The options will be applied to the ortOptions struct and the cudaOptionsSet flag will be set to true.
+func WithCuda(options map[string]string) WithOption {
+	return func(o *ortOptions) {
+		o.cudaOptions = options
+		o.cudaOptionsSet = true
+	}
+}
+
+// WithCoreML Use this function to set the CoreML options flags for the ONNX Runtime configuration.
+// The `flags` parameter represents the CoreML options flags.
+// The `o.coreMLOptions` field in `ortOptions` struct will be set to the provided flags parameter.
+// The `o.coreMLOptionsSet` field in `ortOptions` struct will be set to true.
+func WithCoreML(flags uint32) WithOption {
+	return func(o *ortOptions) {
+		o.coreMLOptions = flags
+		o.coreMLOptionsSet = true
+	}
+}
+
+// WithDirectML Use this function to set the DirectML device ID for the
+// onnxruntime. By default, this option is not set.
+func WithDirectML(deviceID int) WithOption {
+	return func(o *ortOptions) {
+		o.directMLOptions = deviceID
+		o.directMLOptionsSet = true
+	}
+}
+
+// WithOpenVINO Use this function to set the OpenVINO options for the OpenVINO execution provider.
+// The options parameter should be a map of string keys and string values, representing the configuration options.
+// For each key-value pair in the map, the specified option will be set in the OpenVINO execution provider.
+// Once the options are set, the openVINOOptionsSet flag in the ortOptions struct will be set to true.
+// Example usage: WithOpenVINO(map[string]string{"device_type": "CPU", "num_threads": "4"})
+// This will configure the OpenVINO execution provider to use CPU as the device type and set the number of threads to 4.
+func WithOpenVINO(options map[string]string) WithOption {
+	return func(o *ortOptions) {
+		o.openVINOOptions = options
+		o.openVINOOptionsSet = true
+	}
+}
+
+// WithTensorRT Use this function to set the options for the TensorRT provider.
+// The options parameter should be a pointer to an instance of TensorRTProviderOptions.
+// By default, the options will be nil and the TensorRT provider will not be used.
+// Example usage:
+//
+//	options := &onnxruntime_go.TensorRTProviderOptions{
+//	    DeviceID: 0,
+//	}
+//	WithTensorRT(options)
+//
+// Note: For the TensorRT provider to work, the onnxruntime library must be built with TensorRT support.
+func WithTensorRT(options map[string]string) WithOption {
+	return func(o *ortOptions) {
+		o.tensorRTOptions = options
+		o.tensorRTOptionsSet = true
 	}
 }
