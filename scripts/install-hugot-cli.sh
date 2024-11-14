af5733e34440d106fc61910d2dccb9aef4fe77c4
austinjp <austinjp@users.noreply.github.com>
2024-11-14 12:00:10 +0000

    Always download latest version
    The var hugot_version was outdated (it was v0.0.9 instead of v0.1.8) which made the script download Github's 404 page instead of the correct files. This edit removes the explicit hugot_version and instead downloads from the latest release.

--- scripts/install-hugot-cli.sh
+++ scripts/install-hugot-cli.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-hugot_version=v0.0.9
-
-echo "Installing hugot cli version $hugot_version..."
+echo "Installing hugot cli..."
 
 mkdir -p "$HOME/lib/hugot"
 mkdir -p "$HOME/.local/bin"
 onnxruntime_path=$HOME/lib/hugot/onnxruntime.so
 hugot_path=$HOME/.local/bin/hugot
 
-curl -L https://github.com/knights-analytics/hugot/releases/download/$hugot_version/onnxruntime-linux-x64.so -o "$onnxruntime_path"
-curl -L https://github.com/knights-analytics/hugot/releases/download/$hugot_version/hugot-cli-linux-x64 -o "$hugot_path"
+curl -L https://github.com/knights-analytics/hugot/releases/latest/download/onnxruntime-linux-x64.so -o "$onnxruntime_path"
+curl -L https://github.com/knights-analytics/hugot/releases/latest/download/hugot-cli-linux-x64 -o "$hugot_path"
 chmod +x "$hugot_path"
 
 echo "onnxruntime.so shared library installed at $onnxruntime_path"
