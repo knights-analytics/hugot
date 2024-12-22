91306c47a4b61e9f5af9e93b4e5a40511da95756
Rob <rob@knightsanalytics.com>
2024-12-22 11:00:38 +0000

    Fix compilation issues with XLA disabled

--- datasets/dataset_xla_disabled.go
+++ datasets/dataset_xla_disabled.go
@@ -4,10 +4,14 @@ package datasets
 
 import "github.com/knights-analytics/hugot/pipelineBackends"
 
-type Dataset interface{}
+type Dataset interface {
+	SetVerbose(bool)
+}
 
 type SemanticSimilarityDataset struct{}
 
 func (s *SemanticSimilarityDataset) SetTokenizationPipeline(_ pipelineBackends.Pipeline) error {
 	return nil
 }
+
+func (s *SemanticSimilarityDataset) SetVerbose(_ bool) {}
--- pipelineBackends/model_xla_disabled.go
+++ pipelineBackends/model_xla_disabled.go
@@ -14,10 +14,14 @@ func createXLAModelBackend(_ *Model, _ *options.Options) error {
 	return nil
 }
 
-func createInputTensorsXLA(_ *PipelineBatch, _ []InputOutputInfo) error {
+func createInputTensorsXLA(_ *PipelineBatch, _ []InputOutputInfo, _ bool) error {
 	return nil
 }
 
 func runXLASessionOnBatch(_ *PipelineBatch, _ *BasePipeline) error {
+	return nil
+}
+
+func (xlaModel *XLAModel) Save(_ string) error {
 	return nil
 }
