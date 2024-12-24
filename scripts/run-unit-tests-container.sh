4afc336bfde23ef25d4defbacae845a8ff413480
riccardopinosio <rpinosio@gmail.com>
2024-12-24 12:47:06 +0100

    fixing tests

--- scripts/run-unit-tests-container.sh
+++ scripts/run-unit-tests-container.sh
@@ -4,9 +4,18 @@ set -e
 
 cd /build && \
 mkdir -p /test/unit && \
-go run ./testData/downloadModels.go && \
-gotestsum --junitfile=/test/unit/unit.xml --jsonfile=/test/unit/unit.json -- -tags=ALL -coverprofile=/test/unit/cover.out.pre -race -covermode=atomic ./...
-grep -v "downloadModels.go" /test/unit/cover.out.pre > /test/unit/cover.out
-rm  /test/unit/cover.out.pre
+go run ./testData/downloadModels.go
+
+echo HUGOT_BUILD_TAG is "$HUGOT_BUILD_TAG"
+
+if [[ -n $HUGOT_BUILD_TAG ]]; then 
+    echo "running with -tags=ALL"
+    gotestsum --junitfile=/test/unit/unit.xml --jsonfile=/test/unit/unit.json -- -tags=ALL -coverprofile=/test/unit/cover.out.pre -race -covermode=atomic ./...
+else 
+    echo "running without build tags"
+    gotestsum --junitfile=/test/unit/unit.xml --jsonfile=/test/unit/unit.json -- -coverprofile=/test/unit/cover.out.pre -race -covermode=atomic ./...
+fi
+
+grep -v "downloadModels.go" /test/unit/cover.out.pre > /test/unit/cover.out && rm  /test/unit/cover.out.pre
 
 echo Done.
