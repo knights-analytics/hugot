ea10fa9932745b1a77cef2c663f90680ec7204b2
Rob Keevil <robkeevil@gmail.com>
2025-02-24 15:05:53 +0100

    switch to goMLX huggingface downloader

--- downloader.go
+++ downloader.go
@@ -3,23 +3,22 @@
 package hugot
 
 import (
-	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
-	"net/http"
 	"path"
 	"path/filepath"
 	"strings"
 	"time"
 
-	hfd "github.com/bodaay/HuggingFaceModelDownloader/hfdownloader"
+	"github.com/gomlx/go-huggingface/hub"
+
+	"github.com/knights-analytics/hugot/util"
 )
 
 // DownloadOptions is a struct of options that can be passed to DownloadModel.
 type DownloadOptions struct {
 	AuthToken             string
-	SkipSha               bool
+	OnnxFilePath          string
 	Branch                string
 	MaxRetries            int
 	RetryInterval         int
@@ -41,11 +40,6 @@ func NewDownloadOptions() DownloadOptions {
 // DownloadModel can be used to download a model directly from huggingface. Before the model is downloaded,
 // validation occurs to ensure there is an .onnx and tokenizers.json file. Hugot only works with onnx models.
 func DownloadModel(modelName string, destination string, options DownloadOptions) (string, error) {
-	// make sure it's an onnx model with tokenizer
-	err := validateDownloadHfModel(modelName, options.Branch, options.AuthToken)
-	if err != nil {
-		return "", err
-	}
 
 	// replicates code in hf downloader
 	modelP := modelName
@@ -54,104 +48,122 @@ func DownloadModel(modelName string, destination string, options DownloadOptions
 	}
 	modelPath := path.Join(destination, strings.Replace(modelP, "/", "_", -1))
 
+	repo := hub.New(modelName)
+	if options.AuthToken != "" {
+		repo = repo.WithAuth(options.AuthToken)
+	}
+	if options.ConcurrentConnections > 0 {
+		repo.MaxParallelDownload = options.ConcurrentConnections
+	}
+	if options.Verbose {
+		repo.Verbosity = 1
+		repo.WithProgressBar(true)
+	} else {
+		repo.Verbosity = 0
+		repo.WithProgressBar(false)
+	}
+	if options.Branch != "" {
+		repo.WithRevision(options.Branch)
+	}
+
+	// make sure it's an onnx model with tokenizer
+	downloadFiles, err := validateDownloadHfModel(repo, options)
+	if err != nil {
+		return "", err
+	}
+
 	for i := 0; i < options.MaxRetries; i++ {
-		if err := hfd.DownloadModel(modelName, false, options.SkipSha, false, destination, options.Branch, options.ConcurrentConnections, options.AuthToken, !options.Verbose); err != nil {
+		downloadPaths, downloadErr := repo.DownloadFiles(downloadFiles...)
+		if downloadErr != nil {
 			if options.Verbose {
-				fmt.Printf("Warning: attempt %d / %d failed, error: %s\n", i+1, options.MaxRetries, err)
+				fmt.Printf("Warning: attempt %d / %d failed, error: %s\n", i+1, options.MaxRetries, downloadErr)
 			}
 			time.Sleep(time.Duration(options.RetryInterval) * time.Second)
 			continue
 		}
+
+		for j, downloadPath := range downloadPaths {
+			truePath, symErr := filepath.EvalSymlinks(downloadPath)
+			if symErr != nil {
+				return "", symErr
+			}
+			moveErr := util.CopyFile(truePath, fmt.Sprintf("%s/%s", modelPath, path.Base(downloadFiles[j])))
+			if moveErr != nil {
+				return "", moveErr
+			}
+		}
+
 		if options.Verbose {
 			fmt.Printf("\nDownload of %s completed successfully\n", modelName)
 		}
 		return modelPath, nil
 	}
+
 	return "", fmt.Errorf("failed to download %s after %d attempts", modelName, options.MaxRetries)
 }
 
-type hfFile struct {
-	Type        string `json:"type"`
-	Path        string `json:"path"`
-	IsDirectory bool
-}
+func validateDownloadHfModel(repo *hub.Repo, options DownloadOptions) ([]string, error) {
 
-func validateDownloadHfModel(modelPath string, branch string, authToken string) error {
-	if strings.Contains(modelPath, ":") {
-		return errors.New("model filters are not supported")
+	onnxFilePath := options.OnnxFilePath
+
+	for i := 0; i < options.MaxRetries; i++ {
+		err := repo.DownloadInfo(false)
+		if err != nil {
+			if options.Verbose {
+				fmt.Printf("Warning: list repo attempt %d / %d failed, error: %s\n", i+1, options.MaxRetries, err)
+			}
+			if i+1 == options.MaxRetries {
+				return nil, err
+			}
+			time.Sleep(time.Duration(options.RetryInterval) * time.Second)
+		}
 	}
 
-	client := &http.Client{}
+	tokenizerPath := ""
+	onnxPath := ""
+	var toDownload []string
+	var allOnnx []string
+	for fileName, err := range repo.IterFileNames() {
+		if err != nil {
+			return nil, err
+		}
 
-	hasTokenizer, hasOnxx, err := checkURL(client, fmt.Sprintf("https://huggingface.co/api/models/%s/tree/%s", modelPath, branch), authToken)
-	if err != nil {
-		return err
+		baseFileName := filepath.Base(fileName)
+		if baseFileName == "tokenizer.json" {
+			tokenizerPath = fileName
+		} else if baseFileName == "special_tokens_map.json" ||
+			baseFileName == "tokenizer_config.json" ||
+			baseFileName == "config.json" ||
+			baseFileName == "vocab.txt" {
+			toDownload = append(toDownload, fileName)
+		} else if filepath.Ext(baseFileName) == ".onnx" {
+			if onnxFilePath != "" {
+				if fileName == onnxFilePath {
+					onnxPath = fileName
+				}
+			} else {
+				onnxPath = fileName
+			}
+			allOnnx = append(allOnnx, fileName)
+		}
 	}
 
 	var errs []error
-	if !hasOnxx {
-		errs = append(errs, fmt.Errorf("model does not have a model.onnx file, Hugot only works with onnx models"))
-	}
-	if !hasTokenizer {
+	if tokenizerPath == "" {
 		errs = append(errs, fmt.Errorf("model does not have a tokenizer.json file"))
 	}
-	return errors.Join(errs...)
-}
-
-func checkURL(client *http.Client, url string, authToken string) (bool, bool, error) {
-	var tokenizerFound bool
-	var onnxFound bool
-	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, url, nil)
-	if err != nil {
-		return false, false, err
-	}
-	if authToken != "" {
-		req.Header.Add("Authorization", "Bearer "+authToken)
-	}
-	resp, err := client.Do(req)
-	if err != nil {
-		return false, false, err
-	}
-	defer func(resp *http.Response) {
-		err = errors.Join(err, resp.Body.Close())
-	}(resp)
-
-	var filesList []hfFile
-	e := json.NewDecoder(resp.Body).Decode(&filesList)
-	if e != nil {
-		return false, false, e
-	}
-
-	var dirs []hfFile
-	for _, f := range filesList {
-		if filepath.Base(f.Path) == "tokenizer.json" {
-			tokenizerFound = true
+	if onnxFilePath != "" {
+		if onnxPath == "" {
+			errs = append(errs, fmt.Errorf("model .onnx file not found at %s", onnxFilePath))
 		}
-		if filepath.Ext(f.Path) == ".onnx" {
-			onnxFound = true
-		}
-		if f.Type == "directory" {
-			// Do dirs later if files not found at this level
-			dirs = append(dirs, f)
-		}
-		if onnxFound && tokenizerFound {
-			break
+	} else {
+		numModels := len(allOnnx)
+		if numModels == 0 {
+			errs = append(errs, fmt.Errorf("model does not have a .onnx file, Hugot only works with onnx models"))
+		} else if numModels > 1 {
+			errs = append(errs, fmt.Errorf("model has multiple .onnx files, please specify one of the following onnxFilePaths: %s", strings.Join(allOnnx, " ")))
 		}
 	}
 
-	if !(onnxFound && tokenizerFound) {
-		for _, dir := range dirs {
-			tokenizerFoundRec, onnxFoundRec, dirErr := checkURL(client, url+"/"+dir.Path, authToken)
-			if dirErr != nil {
-				return false, false, dirErr
-			}
-			tokenizerFound = tokenizerFound || tokenizerFoundRec
-			onnxFound = onnxFound || onnxFoundRec
-			if onnxFound && tokenizerFound {
-				break
-			}
-		}
-	}
-
-	return tokenizerFound, onnxFound, nil
+	return append(toDownload, tokenizerPath, onnxPath), errors.Join(errs...)
 }
